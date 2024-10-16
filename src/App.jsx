a08de5d99ead13c30ce23197feba093da4d29013
valentine <valentineojienor14@gmail.com>
2024-10-16 16:10:33 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import Signup from "../src/components/Signup"
 
 function App() {
 
   return (
-    <div className="">
-      <div className="desktop">
+    // <div className="">
+    //   <div className="desktop">
         
-      </div>
-      <div className="mobile">
+    //   </div>
+    //   <div className="mobile">
 
-      </div>
-    </div>
+    //   </div>
+    // </div>
+
+  <Signup />
   )
 }
 
