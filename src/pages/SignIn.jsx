bedd1b300a6f276a86942218ba45aa63edf63abb
promise <uwagboepromise5@gmail.com>
2024-11-22 16:45:43 +0100

    Update on sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -146,7 +146,8 @@ const SignIn = () => {
               {/* Submit Button */}
               <button
                 type="submit"
-                className="w-full rounded-md bg-blue-800 py-2 font-medium text-white hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                // onClick={onSubmit}
+                className="relative cursor-pointer w-full rounded-md bg-blue-800 py-2 font-medium text-white hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 Sign in
               </button>
